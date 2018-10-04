680de41b6503b1114ced19894ffd0def202a7a0c
Max <marc.garcia.u@gmail.com>
2018-10-04 09:40:38 +0200

    nothing

--- ia h_2/Assets/Steering/SteeringAlign.cs
+++ ia h_2/Assets/Steering/SteeringAlign.cs
@@ -21,10 +21,30 @@ public class SteeringAlign : MonoBehaviour {
         // Find the desired rotation and accelerate to it
         // Use Vector3.SignedAngle() to find the angle between two direction
 
-        Vector3 desired_v = (move.target.transform.position - move.transform.position).normalized * move.max_mov_velocity;
+        Vector3 desired_v = (move.target.transform.position - move.transform.position);
         Vector3 axis= new Vector3(0, 1, 0);
         float angle = Vector3.SignedAngle(move.movement, desired_v,axis);
 
-        move.AccelerateRotation(angle);
+        //float distance = (target - move.transform.position).magnitude;
+        if (angle <= min_angle)
+        {
+            move.SetRotationVelocity(0.0f);
+        }
+        else
+        {
+            float acceleration=0;
+            if (angle < slow_angle)
+            {
+                acceleration = move.max_rot_velocity * angle / slow_angle;
+            }
+
+            acceleration /= time_to_target;
+
+            if (angle <0 )
+            {
+                acceleration -=acceleration;
+            }
+            move.AccelerateRotation(angle);
+        }
     }
 }
