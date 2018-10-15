8730c105116102942831c888c38d4e03b5297e3c
Max <marc.garcia.u@gmail.com>
2018-10-15 10:23:15 +0200

    Pathfinding

---
+++ class6/Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class test : MonoBehaviour {
+
+    public GameObject destination;
+    NavMeshAgent patata;
+
+    // Use this for initialization
+    void Start () {
+
+        patata = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        patata.destination = destination.transform.position;
+
+	}
+}
